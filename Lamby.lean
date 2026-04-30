91a68d1c5c62f18649a1ced89091b142168ddfe8
awangelo <anggg.gaio@gmail.com>
2026-04-30 20:43:01 -0300

    Implement basic REPL

--- Lamby.lean
+++ Lamby.lean
@@ -1,5 +1,4 @@
 import Lamby.AST
-import Lamby.Environment
 import Lamby.Parser
 import Lamby.Reduction
 import Lamby.REPL
--- Lamby/REPL.lean
+++ Lamby/REPL.lean
@@ -1,26 +1,44 @@
+import Lamby.Parser
+import Lamby.Reduction
+
 namespace Lamby
 
 inductive Command where
-  | eval (t : String)
-  | help (target : Option String)  -- :help | :help <term> | :h
-  | quit                           -- :quit | :q
+  | help (toDescribe : Option String)
+  | quit
   | unknown (cmd : String)
 
 def parseCommand (input : String) : Command :=
-  let input := input.trim
+  let parts := input.splitOn " " |>.filter (· != "")
 
-  if !input.startsWith ":" then
-    Command.eval input
+  match parts with
+  | [":quit"] | [":q"] => .quit
+  | [":help"] | [":h"] => .help none
+  | [":help", arg]
+  | [":h", arg]        => .help (some arg)
+  | cmd :: _           => .unknown cmd
+  | []                 => .unknown ""
+
+def evalExpression (input : String) : IO Unit := do
+  match parse input with
+  | .error err => .println s!"Error: {err}"
+  | .ok term   => .println $ reduce term
+
+def handleInput (input : String) : IO Unit := do
+  if input.startsWith ":" then
+    match parseCommand input with
+    | .help none     => .println "Available commands\n :quit, :help"
+    | .help (some s) => .println "Available commands\n :quit, :help" -- TODO: help function for commands
+    | .quit          => .println "Bye" ; IO.Process.exit 0
+    | .unknown ""
+    | .unknown ":"   => .println "Unknown command"
+    | .unknown cmd   => .println s!"Unknown command: {cmd.drop 1}"
   else
-    let parts := input.splitOn " " |>.filter (· != "")
+    evalExpression input
 
-    match parts with
-    | [":quit"] | [":q"]           => Command.quit
-    | [":help"] | [":h"]           => Command.help none
-    | [":help", arg] | [":h", arg] => Command.help (some arg)
-    | cmd :: _                     => Command.unknown cmd
-    | []                           => Command.unknown ""
-
-
-
-def loop : IO Unit := IO.println "REPL"
+partial def replLoop : IO Unit := do
+  IO.print "λ> "
+  let stdin ← IO.getStdin
+  let input := (← stdin.getLine).trimAscii.toString
+  handleInput input
+  replLoop
